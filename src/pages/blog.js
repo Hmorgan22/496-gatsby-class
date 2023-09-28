813b6bd388863a070611cb38ca9f4b723cd2b068
Hmorgan22 <huntermorgan1219@yahoo.com>
2023-09-28 22:34:52 +0000

    Updated to part 5 of gatsby tutorial

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,26 +1,31 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+import Seo from '../components/seo'
 
 const BlogPage = ({ data }) => {
   return (
-    <div>{
-      data.allMdx.nodes.map((node) => (
-        <article key={node.id}>
-          <h2>{node.frontmatter.title}</h2>
-          <p>Posted: {node.frontmatter.date}</p>
-          <p>{node.excerpt}</p>
-        </article>
-      ))
-    }</div>
+    <Layout pageTitle="My Blog Posts">
+      {
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
+          </article>
+        ))
+      }
+    </Layout>
   )
 }
+
 export const query = graphql`
   query {
     allMdx(sort: { frontmatter: { date: DESC }}) {
       nodes {
         frontmatter {
+          date(formatString: "MMMM D, YYYY")
           title
-          date(formatString: "MMMM DD, YYYY")
         }
         id
         excerpt
@@ -29,7 +34,6 @@ export const query = graphql`
   }
 `
 
-
-export const Head = () => <title>Blog</title>
+export const Head = () => <Seo title="My Blog Posts" />
 
 export default BlogPage
